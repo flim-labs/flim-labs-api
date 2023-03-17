c007187071c1f27ce808e86151f942002abe61bb
Stefano <stefanotucci89@gmail.com>
2023-03-17 16:32:16 +0100

    fix: Acquire raw data fixed

--- acquire_raw_data.py
+++ acquire_raw_data.py
@@ -1,4 +1,19 @@
+import os.path
+
 from flim_labs_api import FlimLabsApi
 
 api = FlimLabsApi()
-api.acquire_raw_data('firmwares\\photons_tracing_simulator.flim', 'file-test.bin', 32)
+
+filename = 'acquisition.bin'
+firmware = 'firmwares\\photons_tracing_simulator.flim'
+acquisition_size_in_MB = 25
+
+index = 1
+while os.path.exists(filename):
+    extension = os.path.splitext(filename)[1]
+    basename = os.path.splitext(filename)[0]
+    filename = f'{basename}-{index}' + (f'{extension}' if extension != '' else '')
+    index += 1
+    break
+
+api.acquire_raw_data(firmware, filename, acquisition_size_in_MB)
